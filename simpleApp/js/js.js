1536b5fa6e01697d95358c2f48e01b78acf16778
Dom58 <dndahimana58@gmail.com>
2020-08-07 10:11:21 +0200

    learning course from 07/08/2020

---
+++ simpleApp/js/js.js
@@ -0,0 +1,20 @@
+function showHidden() {
+    var division = document.getElementById('showed');
+    // alert('Clicked')
+    // if (division.style.display === "none") {
+        division.style.display = "block";
+    // } else {
+    //     division.style.display = "none";
+    // }
+}
+
+
+function Test() {
+    var test1 = document.getElementById('showed');
+    // alert('Clicked')
+    // if (test1.style.display === "none") {
+    //     test1.style.display = "block";
+    // } else {
+        test1.style.display = "none";
+    // }
+}
